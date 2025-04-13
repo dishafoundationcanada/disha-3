609d49af902e3cc36e8c790c38dbb00d26bb535d
dishafoundationcanada <disha.foundation.ca@gmail.com>
2025-04-13 17:13:34 -0400

    Update index.js

--- src/routes/contact/index.js
+++ src/routes/contact/index.js
@@ -8,25 +8,24 @@ const photographs = (props) => {
 			<div class={style.formWrapper}>
 				<p class={style.pageBody}>
 					<div>Hi!</div>
-					<div>If you are interested in my work and are looking to contact me for a contract please use the following form to contact me.</div>
+					<div>If you are interested in my work and are looking to contact me for a contract please use the form below.</div>
 					<div>&nbsp;</div>
 					<div>Cheers 🍻</div>
 				</p>
-				<form name="contact" method="POST" data-netlify="true" action="/contact/success" data-netlify-honeypot="bot-field">
-					<input type="hidden" name="form-name" value="contact" />
-					<p>
-						<input type="text" name="name" placeholder="Name" required />
-					</p>
-					<p>
-						<input type="email" name="email" placeholder="E-Mail" required />
-					</p>
-					<p>
-						<textarea name="message" placeholder="Message" />
-					</p>
-					<p>
-						<button type="submit">Send</button>
-					</p>
-				</form>
+				<div style={{ width: '100%', overflow: 'hidden' }}>
+					<iframe
+						src="https://docs.google.com/forms/d/e/1FAIpQLSflqwg7uIH0YYl5H-Bq6-ErImqgdbE5po6SAmEbJgIXipm-jQ/viewform?embedded=true"
+						width="100%"
+						height="824"
+						frameborder="0"
+						marginheight="0"
+						marginwidth="0"
+						loading="lazy"
+						style="border: none;"
+					>
+						Loading…
+					</iframe>
+				</div>
 			</div>
 		</div>
 	);
